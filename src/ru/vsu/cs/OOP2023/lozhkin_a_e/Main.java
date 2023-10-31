f248c9a4bdcd689f2f4765bd4dbe5b943e712093
Devonty <Devonty@yandex.ru>
2023-11-01 01:14:12 +0300

    Working ver

---
+++ src/ru/vsu/cs/OOP2023/lozhkin_a_e/Main.java
@@ -0,0 +1,63 @@
+package ru.vsu.cs.OOP2023.lozhkin_a_e;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String sample = "012345678#-#baobab#-#BAOBAB";
+        System.out.println("-".repeat(20));
+
+        System.out.println(StringUtils.indexOf(sample, "NONE"));
+        System.out.println(StringUtils.indexOf(sample, "a", 10));
+        System.out.println(StringUtils.indexOf(sample, "a", 100));
+
+        System.out.println("-".repeat(20) + "split");
+
+        System.out.println(StringUtils.split("--A--AA--AAA--AAAA--AAAAA", "--"));
+
+        System.out.println("-".repeat(20) + "join");
+
+        System.out.println(StringUtils.join(" ", List.of("1", "2", "#")));
+
+        System.out.println("-".repeat(20) + "replace");
+
+        System.out.println(StringUtils.replace("TO_REPLACE 012TO_REPLACE6789 TO_REPLACETO_REPLACE#", "TO_REPLACE", "REPLACED"));
+
+        System.out.println("-".repeat(20) + "equals");
+
+        System.out.println(StringUtils.equals("123", "123"));
+        System.out.println(StringUtils.equals("123", "124"));
+
+        System.out.println("-".repeat(20) + "equalsIgnoreCase");
+
+        System.out.println(StringUtils.equalsIgnoreCase("HAHA", "haha"));
+        System.out.println(StringUtils.equalsIgnoreCase("HAHA#", "haHA#"));
+        System.out.println(StringUtils.equalsIgnoreCase("HAHA", "NanA"));
+
+        System.out.println("-".repeat(20) + "to upper/lower");
+
+        System.out.println(sample);
+        System.out.println(StringUtils.toUpper(sample));
+        System.out.println(StringUtils.toLower(sample));
+
+        System.out.println("-".repeat(20) + "is upper");
+
+        System.out.println(StringUtils.isUpper(sample));
+        System.out.println(StringUtils.isUpper(StringUtils.toUpper(sample)));
+
+        System.out.println("-".repeat(20) + "is lower");
+
+        System.out.println(StringUtils.isLower(sample));
+        System.out.println(StringUtils.isLower(StringUtils.toLower(sample)));
+
+        System.out.println("-".repeat(20) + "trims");
+
+        String toTrim = "    4spaces    ";
+        System.out.println(toTrim);
+        System.out.println("|"  + StringUtils.leftTrim(toTrim) + "|");
+        System.out.println("|"  + StringUtils.rightTrim(toTrim) + "|");
+        System.out.println("|"  + StringUtils.trim(toTrim) + "|");
+
+    }
+
+}
---
+++ src/ru/vsu/cs/OOP2023/lozhkin_a_e/StringUtils.java
@@ -0,0 +1,145 @@
+package ru.vsu.cs.OOP2023.lozhkin_a_e;
+
+import java.util.*;
+
+public class StringUtils {
+
+    public static int indexOf(String string, String toFind, int indexStart) {
+        if (toFind.length() > string.length()) return -1;
+
+        for (int i = indexStart; i < string.length(); i++) {
+            if (equalsStartsIndex(string, i, toFind)) return i;
+        }
+        return -1;
+    }
+
+    public static int indexOf(String string, String toFind) {
+        return indexOf(string, toFind, 0);
+    }
+
+    public static boolean equals(String st1, String st2) {
+        return equals(st1, st2, Alphabets.TO_SAME);
+    }
+
+    private static boolean equals(String st1, String st2, Map<Character, Character> alphabet) {
+        if (st1.length() != st2.length()) return false;
+        return equalsStartsIndex(st1, 0, st2, alphabet);
+    }
+
+    public static boolean equalsStartsIndex(String string, int indexStart, String equalsTo) {
+        return equalsStartsIndex(string, indexStart, equalsTo, Alphabets.TO_SAME);
+    }
+
+    private static boolean equalsStartsIndex(String string, int indexStart, String equalsTo, Map<Character, Character> alphabet) {
+        if (equalsTo.length() > string.length()) return false;
+        if (indexStart + equalsTo.length() > string.length()) return false;
+
+        for (int i = 0; i < equalsTo.length(); i++) {
+            Character c1 = string.charAt(indexStart + i);
+            Character c2 = equalsTo.charAt(i);
+            if (alphabet.getOrDefault(c1, c1) != alphabet.getOrDefault(c2, c2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<String> split(String string, String det) {
+        List<String> lst = new ArrayList<>();
+        int indexStart = 0;
+        int indexEnd = 0;
+        while (indexStart < string.length()) {
+            int tmpI = indexOf(string, det, indexStart);
+            indexEnd = tmpI != -1 ? tmpI : string.length();
+            lst.add(string.substring(indexStart, indexEnd));
+            indexStart = indexEnd + det.length();
+        }
+        return lst;
+    }
+
+    public static String replace(String string, String from, String to) {
+        return join(to, split(string, from));
+    }
+
+    public static <T> String join(String connect, Collection<T> col) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Iterator<T> iterator = col.iterator();
+        while (iterator.hasNext()) {
+            stringBuilder.append(iterator.next().toString());
+            if (iterator.hasNext()) stringBuilder.append(connect);
+        }
+        return stringBuilder.toString();
+    }
+
+    public static boolean equalsIgnoreCase(String st1, String st2) {
+        if (st2 == null) return false;
+        return equalsStartsIndex(st1, 0, st2, Alphabets.TO_LOWER);
+    }
+
+    public static String toUpper(String toUp) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < toUp.length(); i++) {
+            Character c = toUp.charAt(i);
+            stringBuilder.append(Alphabets.TO_UPPER.getOrDefault(c, c));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String toLower(String toUp) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < toUp.length(); i++) {
+            Character c = toUp.charAt(i);
+            stringBuilder.append(Alphabets.TO_LOWER.getOrDefault(c, c));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static boolean isUpper(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            Character c = string.charAt(i);
+            if (Alphabets.TO_UPPER.getOrDefault(c, c) != c) return false;
+        }
+        return true;
+    }
+
+    public static boolean isLower(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            Character c = string.charAt(i);
+            if (Alphabets.TO_LOWER.getOrDefault(c, c) != c) return false;
+        }
+        return true;
+    }
+
+    public static String leftTrim(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            if (!Character.isWhitespace(string.charAt(i))) return string.substring(i);
+        }
+        return "";
+    }
+
+    public static String rightTrim(String string) {
+        for (int i = string.length() - 1; i >= 0; i--) {
+            if (!Character.isWhitespace(string.charAt(i))) return string.substring(0, i);
+        }
+        return "";
+    }
+
+    public static String trim(String string) {
+        int i1 = -1, i2 = -1;
+        for (int i = 0; i < string.length(); i++) {
+            if (!Character.isWhitespace(string.charAt(i))) {
+                i1 = i;
+                break;
+            }
+        }
+        for (int i = string.length() - 1; i >= 0; i--) {
+            if (!Character.isWhitespace(string.charAt(i))) {
+                i2 = i;
+                break;
+            }
+        }
+        if(i1 == -1 || i2 == -1) return "";
+        return string.substring(i1, i2);
+    }
+
+}
